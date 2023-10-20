558211ddea375f86c97be8b7e9f5268d7226e2a1
ChauPM <chaupm.dev@gmail.com>
2023-10-20 23:41:36 +0700

    Fix comperflit

--- Application/QL_BanSach/QL_BanSach/DAL/DBConnection.cs
+++ Application/QL_BanSach/QL_BanSach/DAL/DBConnection.cs
@@ -8,11 +8,6 @@ namespace DAL
 {
     public class DBConnection
     {
-<<<<<<< HEAD
-        public string connectionString = @"Data Source=TRUNGDEPTRAI;Initial Catalog=QLCuaHangSach;Integrated Security=True";
-=======
-        //public string connectionString = @"Data Source=CHAUCUTEVL\CHAUDZHOST;Initial Catalog=QLCuaHangSach;Integrated Security=True";
-        public string connectionString = @"Data Source=HAIDEPTRAI\HAIDEPTRAI;Initial Catalog=QLCuaHangSach;Integrated Security=True";
->>>>>>> 9f01a850fad05b6e1b32c98277abc281194ab399
+        public string connectionString = @"Data Source=CHAUCUTEVL\CHAUDZHOST;Initial Catalog=QLCuaHangSach;Integrated Security=True";
     }
 }
